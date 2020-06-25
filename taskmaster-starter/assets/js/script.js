2d988448c4d3da250bbd738aa3bc60a13840bade
Desert Python <mattsenneff@aol.com>
2020-06-24 19:49:07 -0700

    Jquery ui

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -195,24 +195,56 @@ $(".card .list-group").sortable({
   update: function(event) {
     // loop over current set of children in sortable list
     // loop over current set of children in sortable list
-    $(this).children().each(function() {
-    var text = $(this)
+  // array to store the task data in
+  var tempArr = [];
+
+  // loop over current set of children in sortable list
+  $(this).children().each(function() {
+  var text = $(this)
     .find("p")
     .text()
     .trim();
 
-    var date = $(this)
+  var date = $(this)
     .find("span")
     .text()
     .trim();
 
-    console.log(text, date);
-    });
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+   // trim down list's ID to match object property
+  var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+  // update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+  });
+});
+
+  console.log(tempArr);
   },
   out: function(event) {
     console.log("out", event.target);
   },
   update: function(event) {
     console.log($(this).children());
-  }
-});
+  };
+  $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      console.log("drop");
+      ui.draggable.remove();
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
